e8a291675f85e796f34f8a69b789553bb49f220b
changyeonHeo <qnzlckddus@naver.com>
2025-01-24 17:15:27 +0900

    첫번째 커밋

---
+++ src/main/java/com/example/demo/PersonalBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalBoardApplication.class, args);
+	}
+
+}
